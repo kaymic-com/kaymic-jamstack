3d05d917f2ce065dcacfd92750c1f4ddaa280b96
Kevin McCarthy <kjmagic13@gmail.com>
2023-03-31 10:39:02 -0400

    add form-name field to static form

--- components/global/ContactForm.vue
+++ components/global/ContactForm.vue
@@ -2,10 +2,9 @@
   <form
     name="contact"
     method="POST"
-    action="/"
-    class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
     data-netlify="true"
     data-netlify-honeypot="bot-field"
+    class="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
   >
     <input type="hidden" name="form-name" value="contact" />
 
